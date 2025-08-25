ffd02a44c78e8ea85ab99701add9867abda194db
Carlos Piedra <barce90@hotmail.com>
2025-08-24 21:41:38 -0500

    Despliegue con cambios en package.json

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import ArticuloPage from "./articulo/page";
 
 export default function Home() {
   return (
-   <>
-    {/* <h1 className="text-4xl">Bases de Next.js</h1> */}
-    <ArticuloPage />
-   </>
+    <>
+      {/* <h1 className="text-4xl">Bases de Next.js</h1> */}
+      <ArticuloPage />
+    </>
   );
 }
